bc4dd37eb593d9678251a79c40b0083f75fbd706
Eztof <90273638+Eztof@users.noreply.github.com>
2026-02-18 17:39:28 +0000

    Neu

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// base: './' ist für GitHub Pages (Repo-Unterpfad) am robustesten
-export default defineConfig({
-  base: "./",
-  plugins: [react()],
-});
